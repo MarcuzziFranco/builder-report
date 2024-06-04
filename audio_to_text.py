de2af641bd097a7ac016ece90ca534f5cd140516
FrancoMarcuzzi <marcuzzi7franco@gmail.com>
2024-06-03 22:47:10 -0300

    add load model whisper in gpu or cpu

--- audio_to_text.py
+++ audio_to_text.py
@@ -1,12 +1,17 @@
+from sympy import Integer
 import whisper
 import numpy as np
 import os
-
+import torch
 
 class AudioToText():
 
     def __init__(self) -> None:
-        self.modelo =  whisper.load_model("base")
+        # Verificar si CUDA está disponible y seleccionar el dispositivo
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        # Cargar el modelo y moverlo a la GPU si está disponible
+        self.modelo = whisper.load_model("base").to(self.device)
+        #self.modelo =  whisper.load_model("base")
         self.simple_rate = 16000
 
     def load_audio_complete(self,audio_path, sample_rate=16000):
@@ -15,22 +20,25 @@ class AudioToText():
         print(f"Duración del audio: {duracion:.2f} segundos")
         return audio
 
-    def calculate_total_duration_in_seconds(self,list_audio_path,sample_rate=16000):
+    def calculate_total_duration_total_segment(self,list_audio_path,segment_duration=30,sample_rate=16000):
         total_duration_in_seconds = 0
+        total_segment = 0
         for audio_path in list_audio_path:
             audio = whisper.load_audio(audio_path, sr=sample_rate)
-            duracion = len(audio) / sample_rate
-            total_duration_in_seconds += int(duracion)
-        return total_duration_in_seconds
+            duration = len(audio) / sample_rate
+            amount_segment = np.arange(0, duration, segment_duration)
+            #total_duration_in_seconds += int(duration)
+            total_segment += len(amount_segment)
 
+        return total_segment
+    
     def transcribe_segment(self,modelo, audio_segmento):
-        # Asegurarse de que el audio tenga la forma correcta
         audio_segmento = whisper.pad_or_trim(audio_segmento)
         mel = whisper.log_mel_spectrogram(audio_segmento).to(modelo.device)
-        opciones = whisper.DecodingOptions(language="es", without_timestamps=True)
-        resultado = whisper.decode(self.modelo, mel, opciones)
+        opciones = whisper.DecodingOptions(fp16=torch.cuda.is_available(), language="es")
+        resultado = self.modelo.decode(mel, opciones)
         return resultado.text # type: ignore
-
+    
     def transcribe_audio_by_segments(self,audio_path, duracion_segmento=30, sample_rate=16000):
         audio = self.load_audio_complete(audio_path, sample_rate)
         total_duracion = len(audio) / sample_rate
@@ -45,7 +53,7 @@ class AudioToText():
         
         return transcripcion_completa.strip()
 
-    def transcribe_audio_list_by_segments(self,list_audio_path, duracion_segmento=30, sample_rate=16000):
+    def transcribe_audio_list_by_segments(self,list_audio_path,callback_update_progressbar=None, duracion_segmento=30, sample_rate=16000):
         transcriptions_texts = []
         for audio_path in list_audio_path:
             audio = self.load_audio_complete(audio_path, sample_rate)
@@ -60,6 +68,8 @@ class AudioToText():
                 audio_segmento = audio[int(inicio * sample_rate):int(fin * sample_rate)]
                 transcripcion_segmento = self.transcribe_segment(self.modelo, audio_segmento)
                 transcripcion_completa += transcripcion_segmento + " "
+                if callback_update_progressbar != None:
+                    callback_update_progressbar()
         
             transcriptions_texts.append(transcripcion_completa.strip())
             
